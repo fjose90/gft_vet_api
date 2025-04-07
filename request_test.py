0cdd39fbf0d75bf21b4ecf7769503327107a9177
Fernando José <fernando.tecnicoinfo7@gmail.com>
2025-04-07 15:53:22 -0300

    altera teste da home para verificar se retorna status 400

--- request_test.py
+++ request_test.py
@@ -1,8 +1,10 @@
 from rest_framework.test import APIClient
 import pytest
 
-def test_home_returns_200():
+
+@pytest.mark.django_db
+def test_home_returns_400():
 
     client = APIClient()
-    response = client.get('http://127.0.0.1:8000/')
-    assert response.status_code == 200
+    response = client.get('/')
+    assert response.status_code == 400
